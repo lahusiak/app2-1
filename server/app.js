60702dd4817275df1d5a1c93bda329fc7e672e2b
lahusiak <lahusiak@gmail.com>
2015-11-03 14:49:27 -0600

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set("port", process.env.PORT || 6999);
+
+app.get("/*", function(req, res){
+    var file = req.params[0] || "views/index.html";
+    res.sendFile(path.join(__dirname, "./public", file));
+});
+
+app.listen(app.get("port)"), function(){
+    console.log("Meow", app.get("port"));
+});
+
+
+
