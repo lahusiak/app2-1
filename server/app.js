e96d952273a3590f47563d4ab2cbcb07dfe97513
jasontenbrink <jason.tenbrink@gmail.com>
2015-11-03 17:07:17 -0600

    get people from db

--- server/app.js
+++ server/app.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-app.set("port", process.env.PORT || 6999);
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/employeeDb');
+
+var Person = mongoose.model('Person', new Schema({'firstName':String, 'lastName':String, 'salary':String, 'yearsService':String}, {collection:'employeeDb'}));
+
+app.set("port", process.env.PORT || 5000);
+
+app.route('/data').get(function (req, res) {
+              console.log('/data was sent this should show up on the server console');
+
+              Person.find({}, function (err, data) {
+                  res.send(data);
+              });
+            })
+            .delete(function () {
+                  //Person.findByIdAndRemove(req.body.id, function(){});
+            });
 
 app.get("/*", function(req, res){
     var file = req.params[0] || "views/index.html";
     res.sendFile(path.join(__dirname, "./public", file));
 });
 
-app.listen(app.get("port)"), function(){
+app.listen(app.get("port"), function(){
     console.log("Meow", app.get("port"));
 });
-
-
-
--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,3 +1,51 @@
+var employees = [];
+
 $(document).ready(function(){
     console.log("I'm working!");
+    getData();
+
+    $('#peopleContainer').on('click', 'button', deletePerson);
+
 });
+
+function deletePerson() {
+  var deletedPerson = {};
+  deletedPerson.id = $(this).data('id');
+  //
+  console.log(deletedPerson.id);
+  $.ajax({
+    type: 'DELETE',
+    url: '/data',
+    data: deletedPerson,
+    success: function (data) {
+
+    }
+  });
+}
+
+function getData() {
+  $.ajax({
+    type: 'GET',
+    url: '/data',
+    //data
+    success: function (data) {
+      employees = data;
+      console.log(employees);
+      appendToDom();
+    }
+  });
+}
+
+function appendToDom() {
+  for (var i = 0; i < employees.length; i++) {
+    $('#peopleContainer').append('<div class="row">' +
+    '<p class="individual col-md-2">First Name: ' + employees[i].firstName + '</p>' +
+         '<p class="individual col-md-2">Last Name:' + employees[i].lastName + '</p>' +
+         '<p class="individual col-md-2">Salary: ' + employees[i].salary + '</p>' +
+         '<p class="individual col-md-2">Years of Service: ' + employees[i].yearsService + '</p>' +
+         '<button class="btn btn-danger col-md-3" data-id="'+employees[i]._id + '"> Delete </p>' +
+      '</div>');
+
+  }
+
+}
